a3c0fb5f0fcae1b4648c3804dcdaf837bed73230
Sara Meraouche <148572793+sarahm072@users.noreply.github.com>
2025-05-05 17:41:19 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ document.querySelector('#search-icon').onclick =()=>{
     search.classList.toggle('active');
     
 };
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  navbar.classList.toggle('active');
+};
